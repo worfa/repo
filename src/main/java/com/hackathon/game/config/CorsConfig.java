690ad27caf4402214672ee1688a0ae04c7f45f68
Worfa <worfa@mail.ru>
2022-07-03 01:45:51 +0500

    fix (broomGames) cors config

--- src/main/java/com/hackathon/game/config/CorsConfig.java
+++ src/main/java/com/hackathon/game/config/CorsConfig.java
@@ -1,13 +1,8 @@
 package com.hackathon.game.config;
 
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
 @Configuration
@@ -18,7 +13,7 @@ public class CorsConfig implements RepositoryRestConfigurer {
             RepositoryRestConfiguration config, CorsRegistry cors) {
 
         cors.addMapping("/*")
-                .allowedOrigins("*")
+                .allowedOrigins("http://localhost:3000")
                 .allowedMethods("GET", "PUT", "DELETE", "POST")
                 .allowCredentials(false).maxAge(3600);
     }
