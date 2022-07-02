ad135f8808b828d45e47ee3d2a8d98d00aa7a368
Worfa <worfa@mail.ru>
2022-07-03 03:54:10 +0500

    fix (broomGames) hero

--- src/main/java/com/hackathon/game/repository/PropertyDefinitionRepository.java
+++ src/main/java/com/hackathon/game/repository/PropertyDefinitionRepository.java
@@ -1,13 +1,14 @@
 package com.hackathon.game.repository;
 
 import com.hackathon.game.entity.PropertyDefinition;
+import com.hackathon.game.projection.clusterProjection.PropertyDefinitionView;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @CrossOrigin(origins = "*", methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE})
-@RepositoryRestResource(collectionResourceRel = "definition", path = "definition")
+@RepositoryRestResource(excerptProjection = PropertyDefinitionView.class, collectionResourceRel = "definition", path = "definition")
 public interface PropertyDefinitionRepository extends JpaRepository<PropertyDefinition, Long> {
 
     PropertyDefinition findTopByOrderByIdDesc();
